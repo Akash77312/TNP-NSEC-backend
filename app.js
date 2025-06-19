8f5286c2c7dfc1ddf83d41dfbe0d492a903211e0
Akash Raj <akashsingh.77312@gmail.com>
2025-06-19 23:03:56 +0530

    every updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import express from 'express'
-import dotenv from 'dotenv';
-import {connectDb} from './config/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import { connectDb } from "./config/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { errorMiddleware } from "./middlewares/error.js";
@@ -10,11 +10,10 @@ import adminRoutes from "./routes/adminRoutes.js";
 import mcqRoutes from "./routes/mcqRoutes.js";
 import recruiterJAF from "./routes/recruiterJAFRoutes.js";
 import discussionRoutes from "./routes/discussionRoutes.js";
-import { isAuthenticate } from './middlewares/auth.js';
-import PGraphRoutes from './routes/PGraphRoutes.js'
+import { isAuthenticate } from "./middlewares/auth.js";
+import PGraphRoutes from "./routes/PGraphRoutes.js";
 
-
-const app =express(); 
+const app = express();
 
 dotenv.config();
 connectDb();
@@ -26,10 +25,10 @@ app.use(
   })
 );
 
-  // app.use(cors())
-  app.use(cookieParser());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+// app.use(cors())
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/u", userRoutes);
 app.use("/api/v1/u/profile", profileRoutes);
@@ -37,19 +36,18 @@ app.use("/api/v1/admin", adminRoutes);
 app.use("/api/v1/recruiter", recruiterJAF);
 app.use("/api/v1", mcqRoutes);
 app.use("/api/v1", discussionRoutes);
-app.use("/api/v1/admin",PGraphRoutes)
+app.use("/api/v1/admin", PGraphRoutes);
 
-app.get('/protected', isAuthenticate, (req, res) => {
-  
-  res.send({ success: true, message: "Access granted to protected route", user: req.user });
+app.get("/protected", isAuthenticate, (req, res) => {
+  res.send({
+    success: true,
+    message: "Access granted to protected route",
+    user: req.user,
+  });
 });
-
 
 app.use(errorMiddleware);
-const PORT = 4000 || process.env.PORT
-
+const PORT = process.env.PORT || 4000;
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
-
-
